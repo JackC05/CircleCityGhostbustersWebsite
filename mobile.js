7b43289aa5ee38dcab890b4d7e37b4fed9fdcaa4
JackC05 <jestquestgame@gmail.com>
2023-04-11 22:13:07 -0500

    mobile updates

---
+++ mobile.js
@@ -0,0 +1,7 @@
+var w = window.innerWidth;
+
+	const element = document.getElementById("strange");
+	element.innerHTML = "Do you experience feelings of dread <br> in your basement or attic? <br>
+				Have you or your family <br> ever seen a spook, specter, or ghost?<br>
+				If the answer is “yes,”<br>  then don’t wait another minute.<br>
+				Pick up the phone <br> and call the professionals…";
