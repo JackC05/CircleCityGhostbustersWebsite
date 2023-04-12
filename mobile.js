a60970e5c6eae036807f24f919f36f2e8789ce54
JackC05 <jestquestgame@gmail.com>
2023-04-11 22:13:25 -0500

    js gone

--- mobile.js
+++
@@ -1,7 +0,0 @@
-var w = window.innerWidth;
-
-	const element = document.getElementById("strange");
-	element.innerHTML = "Do you experience feelings of dread <br> in your basement or attic? <br>
-				Have you or your family <br> ever seen a spook, specter, or ghost?<br>
-				If the answer is “yes,”<br>  then don’t wait another minute.<br>
-				Pick up the phone <br> and call the professionals…";
